071d3eb7d9ac77756befa9de839e83fb6f01be23
Liskunov <d.liskunov@xlab-game.com>
2023-10-06 01:38:32 +0300

    HW_03.10

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Spawner spawner;
+    public CloudController cloudController;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("X key down");
+            spawner.Spawn();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Z key down");
+            cloudController.Action();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space key down");
+        }
+    }
+}
